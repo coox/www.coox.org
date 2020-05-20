b50fb4bdc3d9a3d97ee579b4ccbacb2a37412225
Eric Redon <eric.redon@coox.org>
2020-05-20 11:51:05 +0200

    Add dark mode support in canvas with smooth transition

--- src/scripts/lib/wave-strip.js
+++ src/scripts/lib/wave-strip.js
@@ -7,10 +7,6 @@ const doesBrowserSupportRequestAnimationFrame = (function initializeDoesBrowserS
   return !!window.requestAnimationFrame;
 })();
 
-// TODO: dark mode support
-// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
-// }
-
 function initializeContext(canvasId) {
   const canvas = document.getElementById(canvasId);
 
@@ -19,38 +15,49 @@ function initializeContext(canvasId) {
   return canvas.getContext('2d', { alpha: false });
 }
 
-function drawWaveStripFrame(context) {
+function initializeDrawWaveStripFrame(context) {
   const heroTitleElement = document.getElementById('hero-title');
+  const waveStripColorComputerElement = document.getElementById(
+    'wave-strip-color-computer'
+  );
 
-  const canvasWidth = heroTitleElement.clientWidth;
-  const canvasHeight = heroTitleElement.clientHeight;
+  return function drawWaveStripFrame() {
+    const canvasWidth = heroTitleElement.clientWidth;
+    const canvasHeight = heroTitleElement.clientHeight;
 
-  const halfCanvasHeight = Math.floor(canvasHeight / 2);
+    const halfCanvasHeight = Math.floor(canvasHeight / 2);
 
-  context.canvas.width = canvasWidth;
-  context.canvas.height = canvasHeight;
+    // Override logical canvas DOM element width and height properties
+    context.canvas.width = canvasWidth;
+    context.canvas.height = canvasHeight;
 
-  // Canvas styled width always remains 100%
-  // Canvas styled height is recalculated for CSS transition to happen
-  context.canvas.style.height = `${canvasHeight}px`;
+    // Canvas styled width always remains 100%
+    // Canvas styled height is overridden to trigger CSS transition on resize
+    context.canvas.style.height = `${canvasHeight}px`;
 
-  context.fillStyle = '#111';
-  context.fillRect(0, 0, canvasWidth, canvasHeight);
+    // Read colors from computed style to honor color scheme CSS transitions
+    const waveStripColorComputerElementStyle = window.getComputedStyle(
+      waveStripColorComputerElement
+    );
 
-  context.beginPath();
-  context.moveTo(0, halfCanvasHeight);
-  for (
-    let i = 8 + Math.ceil(16 * Math.random());
-    i < canvasWidth;
-    i += 8 + Math.ceil(16 * Math.random())
-  ) {
-    context.lineTo(i, Math.ceil(canvasHeight * Math.random()));
-  }
-  context.lineTo(canvasWidth - 1, halfCanvasHeight);
+    context.fillStyle = waveStripColorComputerElementStyle.backgroundColor;
+    context.fillRect(0, 0, canvasWidth, canvasHeight);
 
-  context.lineWidth = 4;
-  context.strokeStyle = '#777';
-  context.stroke();
+    context.beginPath();
+    context.moveTo(0, halfCanvasHeight);
+    for (
+      let i = 8 + Math.ceil(16 * Math.random());
+      i < canvasWidth - 8;
+      i += 8 + Math.ceil(16 * Math.random())
+    ) {
+      context.lineTo(i, Math.ceil(canvasHeight * Math.random()));
+    }
+    context.lineTo(canvasWidth - 1, halfCanvasHeight);
+
+    context.lineWidth = 4;
+    context.strokeStyle = waveStripColorComputerElementStyle.color;
+    context.stroke();
+  };
 }
 
 export function drawStillWaveStrip(canvasId) {
@@ -58,8 +65,9 @@ export function drawStillWaveStrip(canvasId) {
     return;
   }
   const context = initializeContext(canvasId);
+  const drawWaveStripFrame = initializeDrawWaveStripFrame(context);
 
-  drawWaveStripFrame(context);
+  drawWaveStripFrame();
 }
 
 export function drawAnimatedWaveStrip(canvasId) {
@@ -67,9 +75,10 @@ export function drawAnimatedWaveStrip(canvasId) {
     return;
   }
   const context = initializeContext(canvasId);
+  const drawWaveStripFrame = initializeDrawWaveStripFrame(context);
 
   function animateWaveStrip() {
-    drawWaveStripFrame(context);
+    drawWaveStripFrame();
     window.requestAnimationFrame(animateWaveStrip);
   }
   animateWaveStrip();
