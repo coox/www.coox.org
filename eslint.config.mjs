92d5f61c120df35be3445088c2723f88de42a005
Éric Redon <eric.redon@coox.org>
2025-01-25 22:42:29 +0100

    feat(tools): Add dev dependency on ESLint and typescript-eslint + config file

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    // This pattern is added after the default patterns, which are `["**/node_modules/", ".git/"]`.
+    // https://eslint.org/docs/latest/use/configure/ignore#ignoring-directories
+    ignores: [".astro/"]
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strict
+);
